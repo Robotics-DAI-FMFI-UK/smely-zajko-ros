e989201636eb007fe883156389981caefb834dd6
Ondrej <o.jariabka@gmail.com>
2017-09-12 18:45:31 +0200

    change nn prediction script and model to CNN
    * changed nn model to new small CNN model which predicts middle pixels
    from extracted patches
    
    * changed prediction script to fit the new CNN model
    
    Signed-off-by: Ondrej <o.jariabka@gmail.com>

--- src/robot_control/src/scripts/camera_prediction.py
+++ src/robot_control/src/scripts/camera_prediction.py
@@ -6,26 +6,27 @@ from sensor_msgs.msg import Image
 from skimage.util.shape import view_as_windows
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
-from smely_zajko_dataset import models
+from keras.models import load_model
 
 bridge = CvBridge()
 
 window = (5, 5)
 stride = 5
 
-model = models.mlp(n_input=75, architecture=[(20, 'sigmoid'), (2, 'softmax')],
-                   metrics=['accuracy'])
-model.load_weights(
-    '/home/zajko/Projects/smely-zajko-ros/src/robot_control/src/scripts/smely_zajko_dataset/mlp_20_sigmoid_2_softmax.hdf5')
+model = load_model('/home/zajko/Projects/smely-zajko-ros/src/robot_control/src/scripts/smely_zajko_dataset/small_middle_cnn.hdf5')
 
 
 def prepare_image(image, window, stride):
     window_x, window_y = window
-    Y = np.asarray(np.squeeze(view_as_windows(image,
+    padd = map(int, np.ceil((window_x / 2.0, window_y / 2.0)))
+    img = np.pad(image, (padd, padd, (0, 0)), 'constant',
+                 constant_values=(0, 0))
+    X = np.asarray(np.squeeze(view_as_windows(img,
                                               (window_x, window_y, 3),
                                               step=stride)))
-    nx, ny, w, h, d = Y.shape
-    return Y.reshape((nx * ny, w * h * d))
+    X = X[1:, 1:, :, :, :]
+    nx, ny, w, h, d = X.shape
+    return np.rollaxis(X.reshape((nx * ny, w, h, d)), 3, start=1)
 
 
 def callback(data):
@@ -38,10 +39,7 @@ def callback(data):
         X = (X - 87.062)
         X = (X / 255.0)
         y_pred = model.predict(X)
-        prediction_mask = (y_pred[:, 1].reshape(48, 64) * 255).astype('uint8')
-
-        # print(prediction_mask.max(), prediction_mask.min(), prediction_mask.mean())
-
+        prediction_mask = (np.reshape(y_pred, (48, 64)) * 255).astype('uint8')
         pub.publish(bridge.cv2_to_imgmsg(prediction_mask))
     except CvBridgeError as e:
         print(e)
